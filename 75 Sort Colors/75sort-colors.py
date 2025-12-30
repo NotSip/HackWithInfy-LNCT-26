25f96d46c5622c90b352cbef3f1ef1a8b9b69808
Akshit Sipaiya <akshusipaiya@gmail.com>
2025-12-30 22:37:12 +0000

    Code challenge submission for question: 75 Sort Colors

---
+++ 75 Sort Colors/75sort-colors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        l = 0
+        m = 0
+        n = len(nums)
+        h = n - 1
+        while m <= h:
+
+            if nums[m] == 0:
+                nums[l], nums[m] = nums[m], nums[l]
+                l += 1
+                m += 1
+            elif nums[m] == 1:
+                m += 1
+
+            else :
+                nums[m], nums[h] = nums[h], nums[m]
+                h -= 1
+
+        return nums
