052b5c2293250ec2d7586ae485eb5fbe1a4469fc
Akshit Sipaiya <136346448+NotSip@users.noreply.github.com>
2026-08-24 04:43:35 +0530

    Time: 341 ms (5.99%), Space: 19.1 MB (57.75%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def fibonacci(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+            return fibonacci(n-1)+fibonacci(n-2)
+
+        return fibonacci(n)
