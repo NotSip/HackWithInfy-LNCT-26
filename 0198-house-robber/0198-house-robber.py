4814a97cd49501207edf43563574c3b192b86786
Akshit Sipaiya <136346448+NotSip@users.noreply.github.com>
2026-09-20 16:23:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (6.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,20 +2,18 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [-1]*n
-        def chori(idx):
-            if dp[idx] != -1:
-                return dp[idx]
+        dp[0] = nums[0]
 
-            if idx == 0:
-                return nums[idx]
-            if idx < 0:
-                return 0
-            adj1 = chori(idx-2)+nums[idx]
-            adj2 = chori(idx-1)
+        for i in range(1,n):
+            if i > 1:
+                pick = nums[i] + dp[i-2]
+            else:
+                pick = nums[i]
 
-            dp[idx] = max(adj1,adj2)
-            return dp[idx]
+            not_pick = dp[i-1]
+
+            dp[i] = max(pick,not_pick)
 
 
-        return chori(n-1)
+        return dp[-1]
         
